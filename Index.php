6e99bcd68f5b0abeee169a1c3342187f1e747550
HajagosNorbert <hajagosnorbi@gmail.com>
2018-01-27 17:29:04 +0100

    Kezdeti fázis

---
+++ Index.php
@@ -0,0 +1,12 @@
+<head>
+ <title>Code4Good Project</title>
+</head>
+<body> 
+    <?php 
+    echo"php kód";
+    for($i=0 ; $i < 10 ; $i++){
+        echo ("<br>".$rusult = $i+$i);
+    }
+    ?>
+    <h1>Tesztelgetés</h1>
+</body>
