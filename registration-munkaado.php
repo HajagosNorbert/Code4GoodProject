aaa448954745374634cd19dbd983dfc930a2a8a1
tankerdani <kissdaniboom@gmail.com>
2018-01-29 22:19:34 +0100

    hozzáadva a munkaadó regisztrációs oldal

---
+++ registration-munkaado.php
@@ -0,0 +1,13 @@
+<head>
+    <title>Munkaadó regisztració</title>
+</head>
+<body>
+    <form name="munkaado" method="post">
+        <p>Vezekénév: <input type="text" name="vezeteknev" placeholder="vezetéknév"></p>
+        <p>Keresztnév: <input type="text" name="keresznev" placeholder="keresztnév"></p>
+        <p>Jelszó: <input type="password" name="password" placeholder="jelszó"></p>
+        <p>E-mail cím: <input type="email" name="email" placeholder="email"></p>
+        <p>Telefonszám: <input type="number" name="phonenumber" placeholder="telefonszám"></p>
+        <p><input type="submit" name="submit"></p>
+    </form>
+</body>
