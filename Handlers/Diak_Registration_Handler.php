7e16d762df4af93980e4a1c16e0202cc307ef837
HajagosNorbert <hajagosnorbi@gmail.com>
2018-02-03 00:23:47 +0100

    Ebben a folderben csak a logikára és adatbázisban történő dolgok tárolódnak. Felhasználó számára nem elérhetőek.

---
+++ Handlers/Diak_Registration_Handler.php
@@ -0,0 +1,10 @@
+
+<?php
+
+if(!isset($_POST['daiakRegistrationSubmit'])){
+    echo '<script type="text/javascript"> window.location = "../index.php"</script>';
+} else{
+  
+echo '<h1>Épp dolgoznám fel az infót, ha a lusta programozók megirnának</h1>';
+}
+?>
