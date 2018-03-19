bdfcd0ca648b4e7431c037e338a5771fe75d6df2
HajagosNorbert <hajagosnorbi@gmail.com>
2018-03-19 16:16:50 +0100

    kilistázza a felhasználó értesítéseit.

---
+++ Notificationes.php
@@ -0,0 +1,31 @@
+<?php 
+include_once 'Header.php';
+include_once 'Classes/Notification.php';
+echo '<br><br><br><br><br><br><br><br><br><br>';
+
+if(count($user->notificationIds) === 0){
+    ?>
+        <h1>Mindennel naprakész vagy!<h1>
+    <?php
+}
+    else{
+
+    foreach($user->notificationIds as $notificationId){
+        $notification = new Notification;
+        $notification->setId($notificationId);
+        $notification->setAllFromDB();
+    ?>
+
+    <h2><?= $notification->title ?></h2>
+    <p><?= $notification->content ?></p>
+    <form action="Handlers/Kill_Notification_Handler.php" metho="GET">
+        <input type="submit" name="submit" value="Értettem">
+        <input type="hidden" name="notificationId" value="<?= $notification->id?>">
+    </form>
+    <br><br><br>
+
+    <?php
+    }
+    
+}
+include_once 'Footer.php';
