87074e2319d58ec9ac7e27b79d361f03d66de496
HajagosNorbert <hajagosnorbi@gmail.com>
2018-03-27 20:52:20 +0200

    "feltöltve" oszlop hozzáadva az "ertesitesek" táblához.
    Tettem az értesítések táblába egy időpontot, ami azt jelzi mikor lett feltölte az értesítés.

--- code4good.sql
+++ code4good.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 18, 2018 at 06:20 PM
+-- Generation Time: Mar 27, 2018 at 06:47 PM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -40,17 +40,16 @@ CREATE TABLE IF NOT EXISTS `ajanlatok` (
   `munka_idopont` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `munkaado_id` (`munkaado_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `ajanlatok`
 --
 
 INSERT INTO `ajanlatok` (`id`, `munkaado_id`, `felajanlott_oraszam`, `cim`, `leiras`, `helyszin`, `feltoltve`, `munka_idopont`) VALUES
-(14, 8, 2, 'PC Szerelés', 'Segítesz összepattintani a masinát!', 'SZEGED', '2018-03-08 13:46:00', '2019-03-09 17:30:00'),
 (15, 8, 1, 'Lakás takarítás', 'Segítesz kiporolni a barlangom!', 'Szeged', '2018-03-08 13:48:00', '2018-03-12 14:15:00'),
 (16, 5, 5, 'Kapáni kő!', 'Ammm... Csak kapálj!', 'Szeged', '2018-03-08 13:52:00', '2018-03-15 17:45:00'),
-(17, 8, 1, 'Teafőzés', 'szomjas vagylok', 'Itt', '2018-03-08 16:09:00', '2018-03-01 22:02:00');
+(18, 11, 2, 'Favágás', 'nehéz', 'Szeged', '2018-03-22 16:05:00', '2018-03-23 12:23:00');
 
 -- --------------------------------------------------------
 
@@ -67,17 +66,16 @@ CREATE TABLE IF NOT EXISTS `ajanlatokra_jelentkezesek` (
   PRIMARY KEY (`id`),
   KEY `ajanlat_id` (`ajanlat_id`),
   KEY `jelentkezo_id` (`jelentkezo_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `ajanlatokra_jelentkezesek`
 --
 
 INSERT INTO `ajanlatokra_jelentkezesek` (`id`, `jelentkezo_id`, `ajanlat_id`, `elfogadva`) VALUES
-(18, 9, 14, 0),
-(20, 2, 14, 0),
-(21, 2, 15, 1),
-(23, 9, 16, 0);
+(21, 2, 15, 0),
+(23, 9, 16, 0),
+(26, 12, 18, 1);
 
 -- --------------------------------------------------------
 
@@ -117,18 +115,20 @@ CREATE TABLE IF NOT EXISTS `ertesitesek` (
   `ertesitett_id` int(11) NOT NULL,
   `cim` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `tartalom` text COLLATE utf8mb4_bin NOT NULL,
+  `feltoltve` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
   KEY `ertesitett_id` (`ertesitett_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `ertesitesek`
 --
 
-INSERT INTO `ertesitesek` (`ertesitett_id`, `cim`, `tartalom`, `id`) VALUES
-(3, 'Bajban vagy', 'Nem vitted le a szemetet....!', 1),
-(3, 'Mégnagyobb baj', 'Ma sem vitted le', 2);
+INSERT INTO `ertesitesek` (`ertesitett_id`, `cim`, `tartalom`, `feltoltve`, `id`) VALUES
+(12, 'Meg lettél bízva!', 'Te lettél megbízva a(z) Favágás munkára +3620312 adf által. ', '2018-03-27 19:07:30', 11),
+(2, 'Meg lettél bízva!', 'Te lettél megbízva a(z) Lakás takarítás munkára Big Boss által. ', '2018-03-27 20:28:31', 25),
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-03-27 20:28:35', 26);
 
 -- --------------------------------------------------------
 
@@ -153,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `felhasznalok` (
   `oraszam` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `iskola_id` (`iskola_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `felhasznalok`
@@ -165,7 +165,10 @@ INSERT INTO `felhasznalok` (`id`, `keresztnev`, `vezeteknev`, `email`, `jelszo`,
 (4, 'Anna', 'Fedora', 'uuuiio@gmail.com', 'asd123', 'jjjjjfffa', 0, NULL, NULL, NULL, 1, '44444567891', NULL),
 (5, 'Józsi', 'Magos', 'nincs@freemail.com', 'ammmmm', 'adfdaf', 1, NULL, NULL, 'Nálam fogtok dolgozni kölkök.', NULL, NULL, 10),
 (8, 'Boss', 'Big', 'munka', 'asd', 'később megoldani', 1, '+36301234567', 'NULL', 'NULL', NULL, NULL, 5),
-(9, 'Katalin', 'Fehér', 'diak', 'asd', 'később megoldani', 0, 'NULL', 'NULL', 'Egy szorgos lány vagyok, aki szereti a gyerekeket és mindig alapos munkát végzek.', 1, '7365862159', NULL);
+(9, 'Katalin', 'Fehér', 'diak', 'asd', 'később megoldani', 0, 'NULL', 'NULL', 'Egy szorgos lány vagyok, aki szereti a gyerekeket és mindig alapos munkát végzek.', 1, '7365862159', NULL),
+(10, 'deleteme', '+3620deleteme', 'deleteme@gmail.com', 'deleteme', 'Később megoldani', 0, NULL, 'Később megoldani', 'NULL', 1, '1', NULL),
+(11, 'adf', '+3620312', 'm@gmail.com', 'asd', 'Később megoldani', 1, NULL, 'Később megoldani', 'NULL', NULL, NULL, 5),
+(12, 'sdg', '+3620235', 'd@gmail.com', 'asd', 'Később megoldani', 0, NULL, 'Később megoldani', 'NULL', 3, '123', NULL);
 
 -- --------------------------------------------------------
 
