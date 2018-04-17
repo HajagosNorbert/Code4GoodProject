efb94566facb12648958bca1c1444bc5292ce109
HajagosNorbert <hajagosnorbi@gmail.com>
2018-04-17 13:51:12 +0200

    Az ajánlatoknak van lejárt és elvégzett állapota

--- code4good.sql
+++ code4good.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 12, 2018 at 06:59 PM
+-- Generation Time: Apr 17, 2018 at 11:48 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -38,18 +38,19 @@ CREATE TABLE IF NOT EXISTS `ajanlatok` (
   `helyszin` varchar(500) COLLATE utf8mb4_bin NOT NULL,
   `feltoltve` datetime DEFAULT NULL,
   `munka_idopont` datetime DEFAULT NULL,
+  `lejart` tinyint(1) NOT NULL DEFAULT '0',
+  `elvegzett` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `munkaado_id` (`munkaado_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `ajanlatok`
 --
 
-INSERT INTO `ajanlatok` (`id`, `munkaado_id`, `felajanlott_oraszam`, `cim`, `leiras`, `helyszin`, `feltoltve`, `munka_idopont`) VALUES
-(15, 8, 1, 'Lakás takarítás', 'Segítesz kiporolni a barlangom!', 'Szeged', '2018-03-08 13:48:00', '2018-03-12 14:15:00'),
-(16, 5, 5, 'Kapáni kő!', 'Ammm... Csak kapálj!', 'Szeged', '2018-03-08 13:52:00', '2018-03-15 17:45:00'),
-(18, 11, 2, 'Favágás', 'nehéz', 'Szeged', '2018-03-22 16:05:00', '2018-03-23 12:23:00');
+INSERT INTO `ajanlatok` (`id`, `munkaado_id`, `felajanlott_oraszam`, `cim`, `leiras`, `helyszin`, `feltoltve`, `munka_idopont`, `lejart`, `elvegzett`) VALUES
+(16, 5, 5, 'Kapáni kő!', 'Ammm... Csak kapálj!', 'Szeged', '2018-03-08 13:52:00', '2018-03-15 17:45:00', 0, 0),
+(18, 11, 2, 'Favágás', 'nehéz', 'Szeged', '2018-03-22 16:05:00', '2018-03-23 12:23:00', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -66,17 +67,15 @@ CREATE TABLE IF NOT EXISTS `ajanlatokra_jelentkezesek` (
   PRIMARY KEY (`id`),
   KEY `ajanlat_id` (`ajanlat_id`),
   KEY `jelentkezo_id` (`jelentkezo_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `ajanlatokra_jelentkezesek`
 --
 
 INSERT INTO `ajanlatokra_jelentkezesek` (`id`, `jelentkezo_id`, `ajanlat_id`, `elfogadva`) VALUES
-(21, 2, 15, 0),
 (26, 12, 18, 1),
-(30, 9, 15, 0),
-(32, 9, 16, 0);
+(34, 9, 16, 0);
 
 -- --------------------------------------------------------
 
@@ -92,9 +91,9 @@ CREATE TABLE IF NOT EXISTS `ertekelesek` (
   `megjegyzes` text COLLATE utf8mb4_bin NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `ertekelo_id` (`ertekelo_id`),
-  KEY `ertekelt_id` (`ertekelt_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  KEY `ertekelt_id` (`ertekelt_id`),
+  KEY `ertekelo_id` (`ertekelo_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `ertekelesek`
@@ -103,7 +102,10 @@ CREATE TABLE IF NOT EXISTS `ertekelesek` (
 INSERT INTO `ertekelesek` (`ertekelt_id`, `ertekelo_id`, `ertekeles`, `megjegyzes`, `id`) VALUES
 (2, 3, 2, 'Nagyol alulteljesítettél báttyó....', 1),
 (3, 4, 5, 'Nagyon ügyi voltál <3', 2),
-(9, 8, 4, 'Nagyon szép munkát végzett, habár folyton pimaszkodott.', 3);
+(2, 8, 5, 'Tök jó voltál', 5),
+(9, 8, 5, 'Ez egy rövid vélemény', 8),
+(9, 8, 4, 'Ez egy hosszabb vélemény!Ez egy hosszabb vélemény!Ez egy hosszabb vélemény!Ez egy hosszabb vélemény!Ez egy hosszabb vélemény!Ez egy hosszabb vélemény!Ez egy hosszabb vélemény!', 9),
+(9, 8, 1, 'Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! Ez jóóó hosszú vélemény! ', 10);
 
 -- --------------------------------------------------------
 
@@ -120,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `ertesitesek` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
   KEY `ertesitett_id` (`ertesitett_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `ertesitesek`
@@ -135,7 +137,28 @@ INSERT INTO `ertesitesek` (`ertesitett_id`, `cim`, `tartalom`, `feltoltve`, `id`
 (2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-05 16:26:48', 32),
 (2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-05 18:24:21', 33),
 (2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-05 18:59:29', 34),
-(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-05 18:59:36', 35);
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-05 18:59:36', 35),
+(2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-13 23:51:35', 36),
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-13 23:54:01', 37),
+(2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-13 23:54:05', 38),
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-13 23:54:07', 39),
+(2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-13 23:56:12', 40),
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-13 23:56:18', 41),
+(2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-13 23:56:22', 42),
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-13 23:56:25', 43),
+(2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-13 23:56:40', 44),
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-13 23:56:44', 45),
+(2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-13 23:59:03', 46),
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-13 23:59:14', 47),
+(2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-16 16:16:49', 48),
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-16 16:17:04', 49),
+(2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-16 16:17:07', 50),
+(2, 'Megbízás megszűntetve', 'A(z) Lakás takarítás munkára visszavonták a megbízásod.', '2018-04-16 16:47:12', 51),
+(2, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z) Lakás takarítás munkára', '2018-04-16 21:02:46', 52),
+(9, 'Megbízást kaptál!', 'Te kaptad a megbízást a(z)  munkára', '2018-04-16 23:44:41', 53),
+(2, 'Munka megszűnve', 'A(z) Lakás takarítás munkát ,amire jelentkeztél megszűntentték.', '2018-04-17 00:59:37', 54),
+(9, 'Munka megszűnve', 'A(z) Lakás takarítás munkát ,amire jelentkeztél megszűntentték.', '2018-04-17 00:59:37', 55),
+(2, 'Munka megszűnve', 'A(z) Lakás takarítás munkát ,amire jelentkeztél megszűntentték.', '2018-04-17 00:59:37', 56);
 
 -- --------------------------------------------------------
 
@@ -172,7 +195,7 @@ INSERT INTO `felhasznalok` (`id`, `keresztnev`, `vezeteknev`, `email`, `jelszo`,
 (3, 'Jacob', 'Hoodie', 'sssdffee@gmail.com', 'asd123', 'fggggggfa', 0, 0, NULL, NULL, NULL, 2, '11234555591', NULL),
 (4, 'Anna', 'Fedora', 'uuuiio@gmail.com', 'asd123', 'jjjjjfffa', 0, 0, NULL, NULL, NULL, 1, '44444567891', NULL),
 (5, 'Józsi', 'Magos', 'nincs@freemail.com', 'ammmmm', 'adfdaf', 0, 1, NULL, NULL, 'Nálam fogtok dolgozni kölkök.', NULL, NULL, 10),
-(8, 'Boss', 'Big', 'munka', 'asd', 'később megoldani', 0, 1, '+36301234567', 'NULL', 'NULL', NULL, NULL, 5),
+(8, 'Boss', 'Big', 'munka', 'asd', 'később megoldani', 1, 1, '+36301234567', 'NULL', 'NULL', NULL, NULL, 5),
 (9, 'Katalin', 'Fehér', 'diak@gmail.com', 'asd', 'később megoldani', 1, 0, '+36201234567', 'NULL', 'Egyszerű bemutatkozás', 1, '7365862159', NULL),
 (11, 'adf', '+3620312', 'm@gmail.com', 'asd', 'Később megoldani', 0, 1, NULL, 'Később megoldani', 'NULL', NULL, NULL, 5),
 (12, 'sdg', '+3620235', 'd@gmail.com', 'asd', 'Később megoldani', 0, 0, NULL, 'Később megoldani', 'NULL', 3, '123', NULL);
